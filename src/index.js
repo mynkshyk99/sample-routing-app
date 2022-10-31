96858c5e519bce4d0e2221ce7352bf26d7824d2f
noobtoproo <72166706+noobtoproo@users.noreply.github.com>
2022-10-31 22:21:09 +0530

    update index

--- src/index.js
+++ src/index.js
@@ -6,10 +6,23 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+import {Formio} from 'formiojs';
+const Webform = Formio.Webform;
+const WebformBuilder = Formio.WebformBuilder;
+const Wizard = Formio.Wizard;
+const WizardBuilder = Formio.WizardBuilder;
+
+export {
+  Webform,
+  WebformBuilder,
+  Wizard,
+  WizardBuilder,
+};
+
+export * from './components';
+export * from './constants';
+export * from './modules';
+export * from './types';
+export * from './utils';
+export {Components, Formio, Utils, Templates} from 'formiojs';
 registerServiceWorker();
